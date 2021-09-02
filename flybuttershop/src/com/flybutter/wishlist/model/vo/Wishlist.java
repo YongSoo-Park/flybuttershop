e38b8a48dcb09fbee6a205d29740a2f971e4173c
DESKTOP-MJI1OJ9\Hyoeun <hyobbang12@gmail.com>
2021-09-02 15:33:13 +0900

    21-09-02 15:32 Wishlist vo 수정입니다.

--- flybuttershop/src/com/flybutter/wishlist/model/vo/Wishlist.java
+++ flybuttershop/src/com/flybutter/wishlist/model/vo/Wishlist.java
@@ -2,16 +2,48 @@ package com.flybutter.wishlist.model.vo;
 
 public class Wishlist {
 
+	private int wish_no;
+	private String pCode;
+	private int user_No;
 
+	public Wishlist() {
+		
+	}
+
+	public Wishlist(int wish_no, String pCode, int user_No) {
+		super();
+		this.wish_no = wish_no;
+		this.pCode = pCode;
+		this.user_No = user_No;
+	}
+
+	public int getWish_no() {
+		return wish_no;
+	}
+
+	public void setWish_no(int wish_no) {
+		this.wish_no = wish_no;
+	}
 
 	public String getpCode() {
 		return pCode;
 	}
 
-
 	public void setpCode(String pCode) {
 		this.pCode = pCode;
 	}
 
+	public int getUser_No() {
+		return user_No;
+	}
 
+	public void setUser_No(int user_No) {
+		this.user_No = user_No;
+	}
+
+	@Override
+	public String toString() {
+		return "Wishlist [wish_no=" + wish_no + ", pCode=" + pCode + ", user_No=" + user_No + "]";
+	}
+	
 }
