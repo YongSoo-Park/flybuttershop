e0f6ef32003531137ee93de8e32acd3ac5f18c0d
YongSoo-Park <pysangel@naver.com>
2021-09-17 00:12:46 +0900

    21/09/17 0:11 메인페이지 완성

--- flybuttershop/WebContent/index.jsp
+++ flybuttershop/WebContent/index.jsp
@@ -5,14 +5,19 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8">
 <title>Insert title here</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script  src="http://code.jquery.com/jquery-latest.min.js"></script>
 </head>
 <body style="margin: 0 auto">
 	<script type="text/javascript"> 
 		window.onload = function() {
-			location.replace("main.ma");
+			$('#input').submit();
 
 		} 
 	</script>
 	
+	<form action="mainpage.ma" method="get" id="input">
+	<!-- <input value=0 type="text" name="no"> -->
+	</form>
 </body>  
 </html> 
--- flybuttershop/src/com/flybutter/mainpage/controller/mainpageServlet.java
+++ flybuttershop/src/com/flybutter/mainpage/controller/mainpageServlet.java
@@ -1,10 +1,9 @@
-package com.flybutter.dummy.controller;
+package com.flybutter.mainpage.controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,51 +13,57 @@ import javax.servlet.http.HttpSession;
 
 import com.flybutter.dummy.model.service.MemberService;
 import com.flybutter.dummy.model.vo.Member;
-import com.flybutter.mainpage.model.vo.Mainpage;
 import com.flybutter.mainpage.model.service.MainPageService;
+import com.flybutter.mainpage.model.vo.Mainpage;
 import com.flybutter.product.model.vo.Product;
 
 /**
- * Servlet implementation class dummyLoginA
+ * Servlet implementation class mainpageServlet
  */
-@WebServlet("/dummyLoginA.ma")
-public class dummyLoginA extends HttpServlet {
+@WebServlet("/mainpage.ma")
+public class mainpageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public dummyLoginA() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int no = Integer.parseInt(request.getParameter("no"));
-		Member loginMember=null;
-		HashMap<String,Product> RVItemsList = null;;
+	public mainpageServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Member loginMember = null;
+		HashMap<String, Product> RVItemsList = null;
+		int no;
 		ArrayList<Mainpage> saleList = null;
 		ArrayList<Mainpage> newList = null;
 		ArrayList<Mainpage> bestList = null;
 		ArrayList<ArrayList<Mainpage>> saleTotalList = null;
-		if(no==0) {
-			loginMember = new MemberService().loginAdmin(no);
-			loginMember.setREC_PNO("0");
-			loginMember.setMONEY(999999);
-		}else {
-			loginMember = new MemberService().loginMember(no);
-		}
-		if(!loginMember.getREC_PNO().equals("0")) {
-			RVItemsList  = new MainPageService().RVItemsList(loginMember.getREC_PNO());			
+		HttpSession session = request.getSession();
+		loginMember = (Member) session.getAttribute("loginMember");
+		if (loginMember != null) {
+			no = loginMember.getMEM_USER_NO();
+			if (no == 0) {
+				loginMember = new MemberService().loginAdmin(no);
+				loginMember.setREC_PNO("0");
+				loginMember.setMONEY(999999);
+			} else {
+				loginMember = new MemberService().loginMember(no);
+			}
+			if (!loginMember.getREC_PNO().equals("0")) {
+				RVItemsList = new MainPageService().RVItemsList(loginMember.getREC_PNO());
+			}
 		}
 		saleTotalList = new MainPageService().saleTotalList();
 		saleList = new MainPageService().saleList();
 		newList = new MainPageService().newList();
 		bestList = new MainPageService().bestList();
-		HttpSession session = request.getSession();
 		session.setAttribute("loginMember", loginMember);
 		session.setAttribute("RVItemsList", RVItemsList);
 		request.setAttribute("saleList", saleList);
@@ -69,10 +74,14 @@ public class dummyLoginA extends HttpServlet {
 		request.getRequestDispatcher("views/mainpage/mainPage.jsp").forward(request, response);
 	}
 
+	
+
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
